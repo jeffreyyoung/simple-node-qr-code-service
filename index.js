0e678a14cdd4cda43ee8c0439db52f7d5b00c62c
Jeffrey Young <jeyoung@adobe.com>
2018-12-30 23:41:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 5678;
+var QRCode = require("qrcode");
+
+// http://localhost:5678/qrcode.png?data=https://www.youtube.com
+
+app.get("/qrcode.png", (req, res) => {
+    const data = decodeURI(req.query.data);
+    
+    QRCode.toDataURL(data, function(err, base64EncodedPng) {
+        if (err) {
+            res.err(err);
+        }
+        var img = Buffer.from(
+            base64EncodedPng.replace(/^data:image\/png;base64,/, ''),
+            'base64'
+        );
+
+        res.writeHead(200, {
+            "Content-Type": "image/png",
+            "Content-Length": img.length
+        });
+        res.end(img);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ readme.md
@@ -0,0 +1,18 @@
+## A MicroService for generating QR codes on the fly
+
+
+### getting started
+
+`npm install && npm start`
+
+then in some client do the following
+
+```html
+
+<img src="http://localhost:4000/qrcode.png?data=https://www.google.com" />
+
+```
+
+
+example:
+
